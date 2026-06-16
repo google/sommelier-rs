dcc18ba4b7c15c31cb58ab72d690e3cd542f7bc8
kkimdev <503414+kkimdev@users.noreply.github.com>
2026-06-17 00:19:09 +0900

    chore: add local flake.nix with wayland and .envrc for direnv

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  inputs = {
+    nixpkgs.url = "https://flakehub.com/f/DeterminateSystems/nixpkgs-weekly/0.1";
+    flake-utils.url = "github:numtide/flake-utils";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          config.allowUnfree = true;
+        };
+        rust-toolchain = pkgs.rust-bin.nightly.latest.default.override {
+          targets = [ "x86_64-unknown-linux-musl" ];
+          extensions = [ "rust-src" ];
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = [
+            rust-toolchain
+            pkgs.wayland
+            pkgs.libxkbcommon
+          ];
+
+          shellHook = ''
+            export LD_LIBRARY_PATH="${pkgs.wayland}/lib:${pkgs.libxkbcommon}/lib:$LD_LIBRARY_PATH"
+          '';
+        };
+      }
+    );
+}
