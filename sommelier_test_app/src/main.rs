838675aebde34e3a00eb0dade86077a76f3c0e2c
kkimdev <503414+kkimdev@users.noreply.github.com>
2026-06-17 00:30:34 +0900

    feat: add comprehensive event logging for IME debugging
    - Log all raw input events via raw_input_hook (IME, Key, Focus, etc.)
    - Add on_exit handler to log final text buffer state
    - Log text, clipboard, pointer, and scroll events at appropriate levels

--- sommelier_test_app/src/main.rs
+++ sommelier_test_app/src/main.rs
@@ -1,5 +1,5 @@
 use eframe::egui;
-use egui::Ui;
+use egui::{Event, ImeEvent, Ui};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or("info")).init();
@@ -28,7 +28,42 @@ struct App {
     started: std::time::Instant,
 }
 
+impl App {
+    fn log_event(event: &Event) {
+        match event {
+            Event::Ime(ime) => match ime {
+                ImeEvent::Enabled => log::info!("[IME] Enabled"),
+                ImeEvent::Preedit(text) => log::info!("[IME] Preedit: {:?}", text),
+                ImeEvent::Commit(text) => log::info!("[IME] Commit: {:?}", text),
+                ImeEvent::Disabled => log::info!("[IME] Disabled"),
+            },
+            Event::Text(text) => log::info!("[Text] {:?}", text),
+            Event::Key { key, physical_key, pressed, repeat, modifiers } => {
+                log::info!("[Key] key={:?} physical={:?} pressed={} repeat={} mods={:?}",
+                    key, physical_key, pressed, repeat, modifiers);
+            }
+            Event::Copy => log::info!("[Clipboard] Copy"),
+            Event::Cut => log::info!("[Clipboard] Cut"),
+            Event::Paste(text) => log::info!("[Clipboard] Paste: {:?}", text),
+            Event::PointerMoved(pos) => log::trace!("[Pointer] Moved: {:?}", pos),
+            Event::PointerButton { pos, button, pressed, .. } => {
+                log::info!("[Pointer] Button={:?} pos={:?} pressed={}", button, pos, pressed);
+            }
+            Event::PointerGone => log::trace!("[Pointer] Gone"),
+            Event::WindowFocused(focused) => log::info!("[Focus] WindowFocused={}", focused),
+            Event::MouseWheel { delta, .. } => log::info!("[Scroll] delta={:?}", delta),
+            _ => log::trace!("[Event] {:?}", event),
+        }
+    }
+}
+
 impl eframe::App for App {
+    fn raw_input_hook(&mut self, _ctx: &egui::Context, raw_input: &mut egui::RawInput) {
+        for event in &raw_input.events {
+            Self::log_event(event);
+        }
+    }
+
     fn ui(&mut self, ui: &mut Ui, _frame: &mut eframe::Frame) {
         ui.heading("Sommelier IME Test");
         ui.add_space(8.0);
@@ -42,5 +77,9 @@ impl eframe::App for App {
             log::info!("Auto-exit timeout reached. Exiting application.");
             ui.ctx().send_viewport_cmd(egui::ViewportCommand::Close);
         }
+    }
+
+    fn on_exit(&mut self) {
+        log::info!("[OnExit] Application closing. Final text buffer: {:?}", self.text);
     }
 }
