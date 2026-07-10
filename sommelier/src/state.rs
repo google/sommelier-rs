25008649eb46803274d85c323792b0e7da3188e0
Kibeom Kim <503414+kkimdev@users.noreply.github.com>
2026-07-10 23:13:44 +0900

    fix(ime): forward real serial and time in on_keysym

--- sommelier/src/state.rs
+++ sommelier/src/state.rs
@@ -311,6 +311,7 @@ pub struct TextInputState {
     pub enabled: bool,
     pub enabled_changed: bool,
     pub surrounding_text: Option<(String, i32, i32)>,
+    pub host_serial: u32,
     pub content_hint: u32,
     pub content_purpose: u32,
     pub cursor_rect: Option<(i32, i32, i32, i32)>,
